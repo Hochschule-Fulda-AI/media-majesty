d0772d4b1db3fe9099054e9fce1e4cd85b5438eb
The_philosopher <email.shinudonney@gmail.com>
2023-12-10 21:37:58 +0100

    update styles and timezone

--- mediamajesty/core/forms.py
+++ mediamajesty/core/forms.py
@@ -6,13 +6,13 @@ class LoginForm(AuthenticationForm):
     username = forms.CharField(
         widget=forms.TextInput(attrs={
             "placeholder": "Your username",
-            "class": "w-full p-2 my-2 border border-gray-300 rounded-md"
+            "class": "w-full p-2 my-2 border border-gray-300 rounded-sm"
             })
     )
     password = forms.CharField(
         widget=forms.PasswordInput(attrs={
             "placeholder": "Your password",
-            "class": "w-full p-2 my-2 border border-gray-300 rounded-md"
+            "class": "w-full p-2 my-2 border border-gray-300 rounded-sm"
             })
     )
 
@@ -25,24 +25,24 @@ class SignUpForm(UserCreationForm):
     username = forms.CharField(
         widget=forms.TextInput(attrs={
             "placeholder": "Your username",
-            "class": "w-full p-2 my-2 border border-gray-300 rounded-md"
+            "class": "w-full p-2 my-2 border border-gray-300 rounded-sm"
             })
     )
     email = forms.CharField(
         widget=forms.EmailInput(attrs={
             "placeholder": "Your email address",
-            "class": "w-full p-2 my-2 border border-gray-300 rounded-md"
+            "class": "w-full p-2 my-2 border border-gray-300 rounded-sm"
             })
     )
     password1 = forms.CharField(
         widget=forms.PasswordInput(attrs={
             "placeholder": "Your password",
-            "class": "w-full p-2 my-2 border border-gray-300 rounded-md"
+            "class": "w-full p-2 my-2 border border-gray-300 rounded-sm"
             })
     )
     password2 = forms.CharField(
         widget=forms.PasswordInput(attrs={
             "placeholder": "Repeat password",
-            "class": "w-full p-2 my-2 border border-gray-300 rounded-md"
+            "class": "w-full p-2 my-2 border border-gray-300 rounded-sm"
             })
     )
