0405dda7e3c350664c86af2dfee80e9c046b388b
fdai7816 <achraf.boudabous@informatik.hs-fulda.de>
2023-12-12 23:24:27 +0100

    add email validation & dummy posts scripts

--- mediamajesty/items/urls.py
+++ mediamajesty/items/urls.py
@@ -1,12 +1,14 @@
+# urls.py
 from django.urls import path
 from . import views
 
 app_name = "items"
 
 urlpatterns = [
-        path("", views.items, name="items"),
-        path("add/", views.add_new_item, name="add_new_item"),
-        path("<int:id>/", views.index, name="index"),
-        path("<int:id>/edit/", views.edit_item, name="edit_item"),
-        path("<int:id>/delete/", views.delete_item, name="delete_item"),
+    path("", views.items, name="items"),
+    path("add/", views.add_new_item, name="add_new_item"),
+    path("<int:id>/", views.index, name="index"),
+    path("<int:id>/edit/", views.edit_item, name="edit_item"),
+    path("<int:id>/delete/", views.delete_item, name="delete_item"),
+    path("generate_dummy_posts/", views.generate_dummy_posts, name="generate_dummy_posts"),  # Add this line
 ]
--- mediamajesty/items/views.py
+++ mediamajesty/items/views.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 from django.db.models import Q
-
 from .forms import AddNewItemForm, EditItemForm
 from .models import Category, Item
+from faker import Faker
+from django.contrib.auth.models import User  # Add this import at the beginning of the file
+from django.contrib.auth.decorators import login_required
 
 
 def items(request):
@@ -90,3 +92,32 @@ def delete_item(request, id):
     item = get_object_or_404(Item, id=id, created_by=request.user)
     item.delete()
     return redirect("dashboard:index")
+
+
+def generate_dummy_posts(request):
+    fake = Faker()
+
+    # Create dummy categories
+    category_names = ["Electronics", "Clothing", "Books", "Furniture", "Toys"]
+    categories = [Category.objects.create(name=name) for name in category_names]
+
+    # Create dummy items
+    for _ in range(100):  # Generate 10 dummy posts (adjust as needed)
+        category = fake.random_element(elements=categories)
+        user = User.objects.first()  # You might want to get a specific user here
+
+        item = Item.objects.create(
+            category=category,
+            created_by=user,
+            name=fake.text(50),
+            description=fake.text(),
+            media_url="https://picsum.photos/500/300",
+            price=fake.random_number(2),
+            is_sold=False,
+        )
+
+        # Add a thumbnail image if needed
+        # item.thumbnail = 'C:\Users\achra\Pictures\Predator\Planet9_Wallpaper_5000x2813.jpg'
+        # item.save()
+
+    return render(request, 'items/index.html', {'message': 'Dummy posts added successfully'})
