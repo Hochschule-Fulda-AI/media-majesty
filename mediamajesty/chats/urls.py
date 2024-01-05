111c1a0b3ab8ae01e8760f6620561385dad03e09
Abbas Rabbani <mail.abbas.rabbani@gmail.com>
2024-01-06 00:37:02 +0500

    add asynchronous chat functionality

--- mediamajesty/chats/urls.py
+++ mediamajesty/chats/urls.py
@@ -6,6 +6,6 @@ app_name = "chats"
 
 urlpatterns = [
         path("", views.index, name="index"),
-        path("chat/<int:conversation_id>", views.chat, name="chat"),
-        path("new/<int:item_id>", views.new, name="new"),
+        path("new/<int:item_id>/", views.new, name="new"),
+        path("<int:conversation_id>/", views.chat, name="chat"),
 ]
--- mediamajesty/chats/views.py
+++ mediamajesty/chats/views.py
@@ -9,20 +9,33 @@ from .models import Conversation, Message
 @login_required
 def index(request):
     user = request.user
-    conversations = Conversation.objects.filter(Q(buyer=user) | Q(seller=user))
+    conversations = Conversation.objects.filter(
+        Q(buyer=user) | Q(seller=user)
+    ).order_by(
+        "-created_at"
+    )  # todo: update based on last message
     return render(request, "chats/inbox.html", {"conversations": conversations})
 
 
 @login_required
 def chat(request, conversation_id):
-    print(conversation_id)
-    converstaion = get_object_or_404(Conversation, pk=conversation_id)
+    conversation = get_object_or_404(Conversation, pk=conversation_id)
+    user = request.user
+    partner = conversation.buyer if user == conversation.seller else conversation.seller
+    messages = Message.objects.filter(conversation=conversation)[0:10]
+    reversed_messages = []
+    if messages:
+        reversed_messages = reversed(messages) # reverse order to show newest messages at the bottom
+        conversation.last_message = messages[0].content
+        conversation.save()
     return render(
         request,
         "chats/chat.html",
         {
-            "conversation": converstaion,
-            "mesages": [],
+            "conversation": conversation,
+            "user": user,
+            "partner": partner,
+            "messages": reversed_messages,
         },
     )
 
@@ -33,4 +46,4 @@ def new(request, item_id):
     conversation, created = Conversation.objects.get_or_create(
         buyer=request.user, seller=item.created_by, item=item
     )
-    return redirect("chats:chat", conversation_id=conversation.id)
+    return redirect("chats:chat", conversation_id=conversation.id)  # type: ignore
