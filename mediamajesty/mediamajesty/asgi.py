3105771e958189ceb79247127a85ca33ccb3960e
The_philosopher <email.shinudonney@gmail.com>
2024-01-08 07:55:22 +0100

    deployment fix

--- mediamajesty/mediamajesty/asgi.py
+++ mediamajesty/mediamajesty/asgi.py
@@ -1,13 +1,19 @@
 import os
+import django
 
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
-from django.core.asgi import get_asgi_application
-
-from chats.routing import websocket_urlpatterns
-
+# calling django.setup() is required for django channels to work
+# especially in this order before other imports
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mediamajesty.settings")
+django.setup()
+
+
+from channels.auth import AuthMiddlewareStack # noqa
+from channels.routing import ProtocolTypeRouter, URLRouter # noqa
+from channels.security.websocket import AllowedHostsOriginValidator # noqa
+from django.core.asgi import get_asgi_application # noqa
+
+from chats.routing import websocket_urlpatterns # noqa
+
 
 django_asgi_app = get_asgi_application()
 
