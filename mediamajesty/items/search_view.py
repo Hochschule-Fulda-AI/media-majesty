763e8237c297fa9ff31bd3c3135a4798418f554d
The_philosopher <email.shinudonney@gmail.com>
2024-01-14 21:52:39 +0100

    fix search functionality behaviour with pending items

--- mediamajesty/items/search_view.py
+++ mediamajesty/items/search_view.py
@@ -5,7 +5,9 @@ from .models import Item
 
 def search_suggestion(request):
     query = request.GET.get("query", "")
-    suggestions = Item.objects.filter(name__icontains=query).values("id", "name")
+    suggestions = Item.objects.filter(
+        name__icontains=query, is_sold=False, is_approved=True
+    ).values("id", "name")
     suggestions_list = [
         {"id": suggestion["id"], "name": suggestion["name"]}
         for suggestion in suggestions
