4499f860137fb9a54034a3a3dedc168524791a2a
The_philosopher <email.shinudonney@gmail.com>
2024-01-21 23:01:00 +0100

    fix download

--- mediamajesty/items/download_view.py
+++ mediamajesty/items/download_view.py
@@ -1,16 +1,11 @@
 import os
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
-from .models import Item
 from azure.storage.blob import BlobServiceClient
 from django.contrib.auth.decorators import login_required
 
-def download_blob_to_file(blob_service_client: BlobServiceClient, container_name, blob_name, local_path):
-    blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
+from .models import Item
 
-    with open(file=local_path, mode="wb") as local_blob:
-        download_stream = blob_client.download_blob()
-        local_blob.write(download_stream.readall())
 
 @login_required
 def download(request, id):
@@ -21,17 +16,27 @@ def download(request, id):
     AZURE_ACCOUNT_KEY = str(os.getenv("AZURE_ACCOUNT_KEY"))
     AZURE_CONTAINER = str(os.getenv("AZURE_CONTAINER"))
 
-    blob_service_client = BlobServiceClient(account_url=f"https://{AZURE_ACCOUNT_NAME}.blob.core.windows.net", credential=AZURE_ACCOUNT_KEY)
+    blob_service_client = BlobServiceClient(
+        account_url=f"https://{AZURE_ACCOUNT_NAME}.blob.core.windows.net",
+        credential=AZURE_ACCOUNT_KEY,
+    )
 
     container_name = AZURE_CONTAINER
-    
-    downloads_folder = os.path.join(os.path.expanduser("~"), 'Downloads')
-    
-    local_path = os.path.join(downloads_folder, blob_name.split("/")[-1]) 
 
-    download_blob_to_file(blob_service_client, container_name, blob_name, local_path)
+    response = HttpResponse(content_type="application/octet-stream")
+    response[
+        "Content-Disposition"
+    ] = f'attachment; filename="{blob_name.split("/")[-1]}"'
 
-    with open(local_path, 'rb') as file:
-        response = HttpResponse(file.read(), content_type='application/octet-stream')
-        response['Content-Disposition'] = f'attachment; filename={os.path.basename(local_path)}'
-        return response
+    blob_client = blob_service_client.get_blob_client(
+        container=container_name, blob=blob_name
+    )
+
+    try:
+        download_stream = blob_client.download_blob()
+        response.write(download_stream.readall())
+    except Exception as e:
+        print(e)
+        response.status_code = 404
+
+    return response
