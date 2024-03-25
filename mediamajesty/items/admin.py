633b6d15816fc96c46ea1eb76020b1a76247e052
The_philosopher <email.shinudonney@gmail.com>
2024-03-25 16:47:30 +0100

    reset migrations and add reports to admin panel

--- mediamajesty/items/admin.py
+++ mediamajesty/items/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Category, Item, ItemFeedback
+from .models import Category, Item, ItemFeedback, ItemReport
 
 admin.site.register(Category)
 admin.site.register(Item)
 admin.site.register(ItemFeedback)
+admin.site.register(ItemReport)
